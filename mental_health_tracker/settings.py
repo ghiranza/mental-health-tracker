9e2256ec57418a2d94cdbe0b7482f182be1276dd
ghiranza <ghiranzathaya@gmail.com>
2024-09-02 19:21:51 +0700

    boom pt 2

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-5o8#5)umifpkf^t834ak1k!1-%b%m&gjvi%d)cia^(lu86o-hc'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
